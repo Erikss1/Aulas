2fd2b7ff940a918869a13315fec0d5dfb81e0b4b
Erikss1 <114835262+Erikss1@users.noreply.github.com>
2024-03-21 16:46:44 -0300

    Add files via upload

---
+++ Aula mudanças de variáveis.py
@@ -0,0 +1,21 @@
+# Mudança de uma variável
+a = "Agora eu entendi tudo."
+b = "Graças ao meu querido porfessor."
+print(a, b)
+print(b)
+# Defindo uma variável com base no valor atual de outro(a)
+c = a + b
+print(c)
+# Definição de múltiplas variáveis com o mesmo valor
+x = y = z = 10
+print(x)
+print(y)
+print(z)
+# Definição de múltiplas variáveis com múltiplos valores
+x, y, z = 10, 20, 30
+print(x)
+print(y)
+print(z)
+# Captação do input do usuário
+nome = str(input("Olá, qual o seu nome?"))
+print("Olá", nome)
