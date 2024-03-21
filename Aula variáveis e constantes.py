7df405ddb9be51061b94dcfcc00ee85158aaf394
Erikss1 <114835262+Erikss1@users.noreply.github.com>
2024-03-21 16:46:57 -0300

    Add files via upload

---
+++ Aula variáveis e constantes.py
@@ -0,0 +1,13 @@
+# Variáveis
+# Definição das constantes e das variáveis
+a = 3
+b = 7
+c = ((a+b)-(b+a))
+# Escreva os valores das constantes e das variáveis
+print(c)
+# Calculo com operadores
+print((a+b)-(b+a))
+print((a-b)*(a+c))
+print((a-c)*(a+b))
+print(a==b) or (b==c)
+
