3d22abb0b1be5977cf11c4e63995501f8dff94d0
Erikss1 <114835262+Erikss1@users.noreply.github.com>
2024-08-08 15:58:30 -0300

    Add files via upload

---
+++ Desafio do BigRoger2.py
@@ -0,0 +1,33 @@
+# Ordenar a lista
+numeros1 = [1, 5, 3, 5, 7, 0, 11]
+print(numeros1)
+numeros1.sort()
+print(numeros1)
+numeros1.reverse()
+print(numeros1)
+
+# Listas
+cores = ["red", "pink", "purple", "black", "brown", "gray"]
+frutas = ["melão", "melancia", "uva", "laranja", "maça"]
+print(frutas + cores)
+
+# Repetir a lista
+numeros2 = [1, 2, 3, 4, 5]
+print(numeros2 *3)
+
+# Separar os números
+numeros3 = [1, 4, 5, 8, 9, 11, 13]
+print("números ímpares:")
+print(1)
+print(5)
+print(9)
+print(11)
+print(13)
+print("número pares:")
+print(4)
+print(8)
+
+# Fatiamento
+numeros4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros4.pop(5)
+print(numeros4[2], numeros4[3], numeros4[4], numeros4[5])
